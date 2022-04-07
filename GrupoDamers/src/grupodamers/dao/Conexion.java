cc7958db2af319f3b3e5c9e823ae1bdccc09bab3
Alberto <monta@LAPTOP-QH1UOLAN.>
2022-04-07 18:46:11 +0200

    Clase conexion

---
+++ GrupoDamers/src/grupodamers/dao/Conexion.java
@@ -0,0 +1,34 @@
+package grupodamers.dao;
+
+import java.sql.*;
+
+public class Conexion {
+	
+	protected Connection conexion;
+	
+	// Datos conexion base datos
+	private final String JDBC_Driver ="";
+	private final String DB_URL="";
+	
+	// Credenciales acceso
+	private final String user="root";
+	private final String password="root";
+	
+	public void conectar() throws Exception {
+		try {
+			conexion = DriverManager.getConnection(DB_URL, user, password);
+			Class.forName(JDBC_Driver);
+		} catch (Exception e) {
+			throw e;
+		}
+	}
+	
+	public void desconectar() throws SQLException {
+		if(conexion != null) {
+			if(!conexion.isClosed()) {
+				conexion.close();
+			}
+		}
+	}
+
+}
